36e15b4b1cea3751174bfc5d3587c1ff6bdd09f5
shikha7124 <shikhaverma7124@gmail.com>
2023-05-05 17:36:34 +0530

    get started with python

---
+++ code0.py
@@ -0,0 +1,15 @@
+# print("Hi")
+# help(print)
+# print("the avg of 4 and 3", (3+4)/2)
+age = 23
+# print("My age is", age)
+# num2 = input("Please enter the no. : ")
+# print("No. enter is:", num2)
+print(type(age))
+n1 = 2
+n2 = 3
+sum = float(n1) + float(n2)
+print(sum)
+first_name = 'akshita'
+last_name = 'semwal'
+print("email :"+ first_name+last_name+"@gmail.com")
