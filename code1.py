330f9970c6f140493e5fbb5a632cbc26be0c27aa
shikha7124 <shikhaverma7124@gmail.com>
2023-05-05 17:34:03 +0530

    first code of python

---
+++ code1.py
@@ -0,0 +1,9 @@
+# create a program to ask user to enter distance in km and convert it into miles (1 mile = 1.6 km)
+# x = float(input("enter the distance"))
+x = input("enter the distance")
+# y = float(x)
+# y = y*1.6
+# miles = float(x) * 1.6365
+# print(round(miles, 3))
+# print(float(x) * 1.6365)
+print(round(float(x) * 1.6365, 3))
