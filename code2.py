0f20b232c04f68bee56f5d8946767e40e0f8e9c4
shikha7124 <shikhaverma7124@gmail.com>
2023-05-20 16:34:25 +0530

    Code 2 and 3 added

---
+++ code2.py
@@ -0,0 +1,14 @@
+# string data type 
+myString = "Hi this is Shikha Verma"
+print(type(myString))
+myString2 = 'My name is "Shikha Verma"'
+print(myString2)
+print(type(myString2))
+print(myString2[-2])
+print(len(myString2))
+print(myString2[len(myString2) - 1]) #last character of the string 
+print(myString2[0:7])
+print(myString2[-13:-1])
+# print("Hello" + 1234)    #error - can only concatenate str
+print("Hello" + str(1234))
+
---
+++ code3.py
@@ -0,0 +1,7 @@
+# create a program to ask user it's first, middle and last name
+
+first = input("Enter your first name : ")
+middle = input("Enter your middle name : ")
+last = input("Enter your last name : ")
+
+print("the initials are " + first +" "+ middle +" "+ last)
