abb637f0a39db78cf825ebc3b13cdf0699097ab9
shikha7124 <shikhaverma7124@gmail.com>
2023-07-04 09:12:27 +0530

    for loop

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+name = "SHIKHA"
+for i in name:
+    print(i)
+
+list1 = ["shikha","nisha", "asshi", "tanu"]
+for element in list1:
+    print(element)
+    for i in element:
+        print(i)
+
+'''Can be used in three ways :
+1. range(3) -> means, from 0 to 3-1
+2. range(3, 9) -> means, from 3 to 9-1
+3. range(2, 12, 2) -> last parameter 2 is showing the jump or difference -> 2, 4, 6, 8, 10'''
