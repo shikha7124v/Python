92b52ae9d8e1e3761f7e74d34dd4e0eac2d5dd47
shikha7124 <shikhaverma7124@gmail.com>
2023-07-13 18:10:56 +0530

    for loop with else

---
+++ for_loop_with_else.py
@@ -0,0 +1,17 @@
+# for i in range(5):
+#     print(i)
+# else:
+#     print("No i")
+
+# j = 10
+# while(j>=1):
+#     print(j)
+#     j = j-1
+# else:
+#     print("Out of range")
+
+for x in range(5):
+    if x == 4:
+        break
+else:                   #content of else will only gonna print after the successfully completion of all loops only
+    print("Sorry no x")
