c691781760fad96dfacbf3d52befd2338e98f904
shikha7124 <shikhaverma7124@gmail.com>
2023-09-17 07:59:37 +0530

    added

---
+++ GFG-Python-Practice/a6_instance.py
@@ -0,0 +1,8 @@
+# Create an instance of a specified class and display the namespace of the said instance 
+class Student:
+    def __init__(self, student_id, student_name, class_name):
+        self.student_id = student_id
+        self.student_name = student_name
+        self.class_name = class_name
+student = Student('v12', 'Jhon Smith', 'V')
+print(student.__dict__)
