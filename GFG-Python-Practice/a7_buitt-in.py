399b260e361e15a18927530ad77d24e14fa5734c
shikha7124 <shikhaverma7124@gmail.com>
2023-09-17 08:05:55 +0530

    added

---
+++ GFG-Python-Practice/a7_buitt-in.py
@@ -0,0 +1,7 @@
+# 'builtins' module provides direct access to all 'built-in' identifiers of Python.
+# Write a Python program that imports the abs() function using the builtins module, displays 
+# the documentation of the abs() function and finds the absolute value of -155
+
+import builtins
+help(builtins.abs)
+print(builtins.abs(-155))
